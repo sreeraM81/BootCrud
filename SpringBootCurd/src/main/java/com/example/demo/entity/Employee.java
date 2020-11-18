a56522b9e65477eafba7336c838da3358c40b08b
sreeram <sreeram0481@gmail.com>
2020-11-18 11:44:46 +0530

    first Commit

---
+++ SpringBootCurd/src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@Table(name="Employee")
+public class Employee {
+	@Id
+	@GeneratedValue
+	private Integer empId;
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpDept() {
+		return empDept;
+	}
+	public void setEmpDept(String empDept) {
+		this.empDept = empDept;
+	}
+	public Double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(Double empSal) {
+		this.empSal = empSal;
+	}
+	private String empName;
+	private String empDept;
+	private Double empSal;
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empDept=" + empDept + ", empSal=" + empSal
+				+ "]";
+	}
+	
+
+}
